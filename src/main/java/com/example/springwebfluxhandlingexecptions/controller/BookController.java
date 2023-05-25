71c7b27663b968c8978ceb1512f3c4ea65730959
jackcat13 <christophehenry11@gmail.com>
2023-05-25 09:53:55 +0200

    setup with springboot 3.0.2

--- src/main/java/com/example/springwebfluxhandlingexecptions/controller/BookController.java
+++ src/main/java/com/example/springwebfluxhandlingexecptions/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.springwebfluxhandlingexecptions.controller;
 
 import com.example.springwebfluxhandlingexecptions.exception.UnAuthorizedException;
 import com.example.springwebfluxhandlingexecptions.model.Book;
+import io.micrometer.context.ContextSnapshot;
 import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,12 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.util.context.ContextView;
 
 import java.util.List;
 
+import static reactor.core.publisher.Mono.error;
+
 @RestController
 @RequestMapping("/api")
-public class lBookController {
+public class BookController {
 
     Logger logger = LoggerFactory.getLogger(BookController.class);
 
@@ -26,7 +30,12 @@ public class lBookController {
     @GetMapping("/book")
     public Mono<ResponseEntity<Book>> getBookById() {
         logger.info("Inside Controller currentSpan: {} ", tracer.currentSpan());
-        return Mono.error(new UnAuthorizedException("Access denied"));
+        return Mono
+                .deferContextual(contextView -> {
+                    ContextSnapshot.captureAll(contextView).setThreadLocals();
+                    new UnAuthorizedException("Access denied");
+                    return null;
+                });
     }
 
     @GetMapping("/books")
